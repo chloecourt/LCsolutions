d5f640701f571045c2242979147042c6888c0f9e
Chloe Court <crc.courtois@gmail.com>
2022-05-24 22:10:14 -0400

    reverseWords.js

--- reverseWords.js
+++ reverseWords.js
@@ -2,23 +2,33 @@ var reverseWords = function(s) {
   let left = 0;
   let right = 0; 
   let final = ''
-  let index;
-  while(right !== s.length) {
+  let index= 0; 
+  while(right < s.length) {  
+    // if right is at ' '
     if (s[right] === ' ') { 
-      index = s[++right];
+      // save index for next word 
+      index = ++right;
+      // right needs to begin at last letter of word
       right = right -1; 
-      while (left < right) {
+      // left < right 
+      while (left <= right) {
         final += s[right--]; 
+        
       }
       left = index; 
       right = index; 
-      if (s[s.length-1] !== final[final.length-1]) {
-        final+=' '; 
-      }
+      // if (s.length !== final.length) {
+      //   final+=' '; 
+      // }
     }
      right++; 
   }
+  right--; 
+  final+=' ';
+  while(right >= left) {
+    final+= s[right--];
+  }
   return final; 
 };
-
+                      // L     RI
 console.log(reverseWords("Let's take LeetCode contest"));
